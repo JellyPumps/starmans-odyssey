99b678808c3495ab121cba47781a872b14340c70
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-03-31 22:26:04 +0400

    Feat: Essential utilites (that give me pain)

--- includes/Math/Matrix4x4.hpp
+++ includes/Math/Matrix4x4.hpp
@@ -4,8 +4,10 @@
 
 #pragma once
 
+#include "Vectors.hpp"
+
+#include <array> // Stack storage
 #include <cmath>
-#include <array>    // Stack storage
 
 #if defined(__x86_64__) || defined(_M_X64)
     #include <immintrin.h>  // SSE/AVX
@@ -19,22 +21,45 @@ namespace Math {
     class Matrix4x4 {
     public:
         // Column-major storage
-        std::array<std::array<float, 4>, 4> data;
+        union {
+            std::array<std::array<float, 4>, 4> data;
+            float elements[16];
+        };
 
         // Constructors
-        Matrix4x4();
+        Matrix4x4() : elements{1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1} {};
         explicit Matrix4x4(const std::array<std::array<float, 4>, 4>& cols);
 
         // Static Factories
-        static Matrix4x4 identity();
+        static Matrix4x4 identity() { return {}; }
         static Matrix4x4 zero();
-
-        // SIMD Accelerated operations
+        
+        // Operations
         Matrix4x4 operator*(const Matrix4x4& other) const;
+        Vector3 operator*(const Vector3& vector3) const;
+        Vector3 transformation_direction(const Vector3& dir) const;
+
+        // Utitlity
+        Matrix4x4 inverse() const;
+        Matrix4x4 transpose() const;
+        float determinant() const;
+        [[nodiscard]] Matrix4x4 multiply_scalar(const Matrix4x4 &other) const;
+        [[nodiscard]] Matrix4x4 multiply_sse(const Matrix4x4 &other) const;
+
+        // Transformations
+        static Matrix4x4 translation(const Vector3& trans);
+        static Matrix4x4 rotation_x(float angle);
+        static Matrix4x4 rotation_y(float angle);
+        static Matrix4x4 rotation_z(float angle);
+        static Matrix4x4 rotationxyz(Vector3 radians);
+        static Matrix4x4 perspective(float fov, float aspect, float near, float far);
+        static Matrix4x4 orthographic(float left, float right, float bottom, float top, float near, float far);
+        static Matrix4x4 look_at(const Vector3& eye, const Vector3& target, const Vector3& up = Vector3(0, 1, 0));
+
 
         // Accessors
-        const float *operator[](size_t col) const { return data[col].data(); }
-        float *operator[](size_t col) { return data[col].data(); }
+        const float* _data() const { return elements; }
+        float* _data() { return elements; }
     };
 }
 
