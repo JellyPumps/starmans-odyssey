b291a5a911620b116ae2772bb87c9b7eaba513f7
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-03-31 20:33:14 +0400

    Feat: Setting up for Camera

--- includes/Rendering/Camera.hpp
+++ includes/Rendering/Camera.hpp
@@ -4,11 +4,23 @@
 
 #pragma once
 
+#include "Math/Vectors.hpp"
+
 namespace Renderer {
 
 class Camera {
+public:
+    //
 private:
     // Vectors
+    Math::Vector3 position_;
+    Math::Vector3 rotation_; // Radians
+
+    // Floats
+    float fov_ = 0; // Field of view
+    float aspect_ratio_ = 0; // Width / Height
+    float near_plane_ = 0; // Near plane distance
+    float far_plane_ = 0; // Far plane distance
 
 };
 
