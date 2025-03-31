00a220d9274306d225bb07ef7b73c43c79278e36
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-03-31 14:41:18 +0400

    Feat: Window class

---
+++ src/Tools/Window.cpp
@@ -0,0 +1,82 @@
+/*
+* Created by Sarthak Rai on 3/31/25.
+*/
+
+#include "Window.hpp"
+
+#include <iostream>
+#include <ostream>
+
+// Constructor & Destructor
+Window::Window(const char* title, const int width, const int height, const Uint32 flags)
+: title_(title), width_(width), height_(height), flags_(flags) {
+    SDL_Init(SDL_INIT_TIMER | SDL_INIT_AUDIO | SDL_INIT_VIDEO);
+
+    const char *video_driver = SDL_GetCurrentVideoDriver();
+    std::cout << "Video driver: " << video_driver << std::endl;
+
+    window_ = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width_, height_, flags_);
+
+    if (!window_) {
+        throw std::runtime_error("SDL_CreateWindow Failed! <" + std::string(SDL_GetError()) + ">");
+    }
+
+    surface_ = get_surface();
+
+    render();
+}
+
+Window::~Window() {
+    if (window_) { SDL_DestroyWindow(window_); }
+}
+
+// Setters
+void Window::toggle_fullscreen() {
+    fullscreen_ = !fullscreen_;
+    if (fullscreen_) {
+        SDL_SetWindowFullscreen(window_,  SDL_WINDOW_FULLSCREEN);
+    } else {
+        SDL_SetWindowFullscreen(window_, 0);
+        SDL_SetWindowSize(window_, width_, height_);
+        SDL_SetWindowBordered(window_, SDL_TRUE);
+    }
+}
+
+void Window::resize(const int width, const int height) {
+    height_ = height;
+    width_ = width;
+    SDL_SetWindowSize(window_, width, height);
+}
+
+void Window::render() const {
+    SDL_FillRect(get_surface(), nullptr,
+        SDL_MapRGB(
+            get_surface()->format, 50, 50, 50
+        )
+    );
+}
+
+void Window::update() const {
+    SDL_UpdateWindowSurface(window_);
+}
+
+// Getters
+SDL_Window *Window::get_context() const {
+    return window_;
+}
+
+SDL_Surface *Window::get_surface() const {
+    return SDL_GetWindowSurface(window_);
+}
+
+int Window::get_width() const {
+    return width_;
+}
+
+int Window::get_height() const {
+    return height_;
+}
+
+bool Window::is_fullscreen() const {
+    return fullscreen_;
+}
---
+++ src/Tools/Window.hpp
@@ -0,0 +1,48 @@
+/*
+* Created by Sarthak Rai on 3/31/25.
+*/
+
+#pragma once
+
+#include <SDL2/SDL.h>
+
+class Window {
+public:
+  Window(const char *title, int width, int height, Uint32 flags);
+  ~Window();
+
+  // Voids
+  void toggle_fullscreen();
+  void resize(int width, int height);
+  void render() const;
+  void update() const;
+
+  // SDL
+  [[nodiscard]] SDL_Window *get_context() const;
+  [[nodiscard]] SDL_Surface *get_surface() const;
+
+  // Ints
+  [[nodiscard]] int get_width() const;
+  [[nodiscard]] int get_height() const;
+
+  // Bools
+  [[nodiscard]] bool is_fullscreen() const;
+private:
+  // Strings
+  const char *title_;
+
+  // SDL
+  SDL_Window *window_;
+  SDL_Surface *surface_;
+  SDL_DisplayMode display_mode_{};
+
+  // Ints
+  int width_;
+  int height_;
+
+  // Bools
+  bool fullscreen_ = false;
+
+  // Uint32
+  Uint32 flags_;
+};
