3cd284a35bded4a3e1c09fd2a98f8a5281514f22
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-04-01 23:23:30 +0400

    Feat: Successful window creation

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,16 @@
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <stdexcept>
+#include "Window.hpp"
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
 
 int main() {
-  // ---- Initialize GLFW ----
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  // ---- Create A Window ----
-  GLFWwindow *window = glfwCreateWindow(800, 600, "Starman's Odyssey", NULL, NULL);
-
-  if (window == NULL) {
-    glfwTerminate();
-    throw std::runtime_error("Failed to create GLFW window");
-  }
-
-  glfwMakeContextCurrent(window);
-
-  // ---- Initialize GLAD ----
-  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    throw std::runtime_error("Failed to initialize GLAD");
-  }
-
-  // ---- Set Viewport ----
-  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  // ---- Create Window ----
+  STARBORN::Window window(800, 600, "Starman's Odyssey");
 
   // ---- Main Loop ----
-  while (!glfwWindowShouldClose(window)) {
-    glfwSwapBuffers(window);
+  while (!glfwWindowShouldClose(window.get_window())) {
+    glfwSwapBuffers(window.get_window());
     glfwPollEvents();
   }
 
