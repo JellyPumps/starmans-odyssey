41e6ebf72ccbf75d922c280b1e8ec0e8cf3da23d
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-04-02 09:16:22 +0400

    Feat: Shader class to load and use shaders

---
+++ include/Shader.hpp
@@ -0,0 +1,44 @@
+/*
+ * Created by Sarthak Rai on 02 Apr 2025.
+*/
+
+#pragma once
+
+#include <glad/glad.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+namespace STARBORN {
+
+class Shader {
+private:
+  static std::string read_shader_file(const char *file_path);
+  static unsigned int compile_shader(const char *shader_code, GLenum shader_type);
+  void create_shader_program(unsigned int vertex, unsigned int fragment);
+public:
+  // ---- Variables ----
+  unsigned int ID{};
+
+  // ---- Constructor & Destructor ----
+  Shader(const char* vertex_path, const char* fragment_path);
+  ~Shader() = default;
+
+  // --- Activate Shader ----
+  void use() const { glUseProgram(ID); };
+
+  // ---- Set Uniforms ----
+  void set_bool(const std::string& name, const bool value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), static_cast<int>(value));
+  };
+  void set_int(const std::string& name, const int value) const {
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+  };
+  void set_float(const std::string& name, const float value) const {
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+  };
+};
+
+} // STARBORN
---
+++ src/Engine/Shader.cpp
@@ -0,0 +1,91 @@
+/*
+ * Created by Sarthak Rai on 02 Apr 2025.
+*/
+
+#include "Shader.hpp"
+
+#include <bits/fs_fwd.h>
+
+namespace STARBORN {
+
+  // ---- Constructor & Destructor ----
+  Shader::Shader(const char *vertex_path, const char *fragment_path) {
+    // ---- Retrieve Vertex & Fragment Source Code ----
+    const std::string vertex_code = read_shader_file(vertex_path);
+    const std::string fragment_code = read_shader_file(fragment_path);
+
+    const char* vshader = vertex_code.c_str();
+    const char* fshader = fragment_code.c_str();
+
+    // ---- Compile Shaders ----
+    const unsigned int vertex = compile_shader(vshader, GL_VERTEX_SHADER);
+    const unsigned int fragment = compile_shader(fshader, GL_FRAGMENT_SHADER);
+
+    // ---- Shader Program ----
+    create_shader_program(vertex, fragment);
+  }
+
+  std::string Shader::read_shader_file(const char *file_path) {
+    std::string file_contents;
+    std::ifstream shader_file;
+
+    shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try {
+      shader_file.open(file_path);
+      std::stringstream shader_stream;
+
+      // ---- Read File's Buffer Contents ----
+      shader_stream << shader_file.rdbuf();
+
+      // ---- Close File Handles ----
+      shader_file.close();
+
+      // ---- Convert Stream Into String ----
+      file_contents = shader_stream.str();
+    } catch (std::ifstream::failure &e) {
+      std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+    }
+
+    return file_contents;
+  }
+
+  unsigned int Shader::compile_shader(const char *shader_code,
+                                      const GLenum shader_type) {
+    int success;
+
+    const unsigned int shader = glCreateShader(shader_type);
+    glShaderSource(shader, 1, &shader_code, nullptr);
+    glCompileShader(shader);
+
+    // ----- Compile Errors ----
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      char info_log[512];
+      glGetShaderInfoLog(shader, 512, nullptr, info_log);
+      throw std::runtime_error(info_log);
+    }
+
+    return shader;
+  }
+
+  void Shader::create_shader_program(const unsigned int vertex,
+                                     const unsigned int fragment) {
+    int success;
+
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+
+    glGetProgramiv(ID, GL_LINK_STATUS, &success);
+    if (!success) {
+      char info_log[512];
+      glGetProgramInfoLog(ID, 512, nullptr, info_log);
+      throw std::runtime_error(info_log);
+    }
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+  }
+} // STARBORN
