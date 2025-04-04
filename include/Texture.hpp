60a51188b4e604323748b4894ea2650f3f787d62
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-04-04 20:27:22 +0400

    Feat: Texture class for handling textures

---
+++ include/Texture.hpp
@@ -0,0 +1,30 @@
+/*
+ * Created by Sarthak Rai on 04 Apr 2025.
+*/
+
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <Shader.hpp>
+
+namespace STARBORN {
+  class Texture {
+  private:
+    // ---- Variables ----
+    unsigned int texture_id_{};
+    int width_{}, height_{}, nr_channels_{};
+    unsigned char *data_{};
+
+    // ---- Setup ----
+    void setup_texture() const;
+    static void generate_texture(const char *filepath);
+  public:
+    explicit Texture(const char *filepath);
+    ~Texture();
+
+    // ---- Methods ----
+    static void opengl_sampler(const Shader &shader);
+    void bind() const;
+  };
+}
---
+++ src/Engine/Texture.cpp
@@ -0,0 +1,53 @@
+#include "Texture.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <stdexcept>
+
+namespace STARBORN {
+  Texture::Texture(const char *filepath) {
+    glGenTextures(1, &texture_id_);
+    glBindTexture(GL_TEXTURE_2D, texture_id_);
+    generate_texture(filepath);
+    setup_texture();
+  }
+
+  Texture::~Texture() {
+    glDeleteTextures(1, &texture_id_);
+  }
+
+  void Texture::setup_texture() const {
+    glBindTexture(GL_TEXTURE_2D, texture_id_);
+
+    // Texture Wrapping
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // Texture Filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  }
+
+  void Texture::generate_texture(const char *filepath) {
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(filepath, &width, &height, &nrChannels, 0);
+
+    if (data) {
+      const GLenum format = (nrChannels == 4) ? GL_RGBA : GL_RGB;
+      glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+      glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+      throw std::runtime_error("Failed to load texture.");
+    }
+
+    stbi_image_free(data);
+  }
+
+  void Texture::opengl_sampler(const Shader &shader) {
+    shader.set_int("texture_id_", 0);
+  }
+
+  void Texture::bind() const {
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture_id_);
+  }
+}
