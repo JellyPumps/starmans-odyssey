ba4c1341a74cfa9f5567e1897f95657f5d5e337c
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-04-05 23:56:10 +0400

    Feat: Working test with camera

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,7 @@
 #include "Shader.hpp"
 #include "Mesh.hpp"
 #include "Texture.hpp"
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+#include "Camera.hpp"
 
 
 int main() {
@@ -70,10 +68,16 @@ int main() {
   };
 
   std::vector cube_positions = {
-    glm::vec3(0.0f, 0.0f, 0.0f),
-    glm::vec3(2.0f, 5.0f, -15.0f),
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
     glm::vec3(-1.5f, -2.2f, -2.5f),
     glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3 (2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
   };
 
   const STARBORN::Mesh mesh(vertices);
@@ -83,6 +87,18 @@ int main() {
 
   shader.use();
   STARBORN::Texture::opengl_sampler(shader);
+
+  glfwSwapInterval(0);
+
+  // ---- Camera ----
+  auto camera_position = glm::vec3(0.0f, 0.0f, 3.0f);
+  auto camera_target = glm::vec3(0.0f, 0.0f, 0.0f);
+
+  STARBORN::Camera camera(camera_position, camera_target, 45.0f,
+    static_cast<float>(window.get_width()) / static_cast<float>(window.get_height()),
+    0.1f, 100.0f);
+
+  shader.set_mat4("projection", camera.get_projection_matrix());
 
   // ---- Main Loop ----
   while (!glfwWindowShouldClose(window.get_window())) {
@@ -96,25 +112,18 @@ int main() {
     shader.use();
 
     // ---- Transformation ----
-    auto view = glm::mat4(1.0f);
-    auto projection = glm::mat4(1.0f);
-
-    projection = glm::perspective(
-      glm::radians(45.0f),
-      static_cast<float>(window.get_width()) / static_cast<float>(window.get_width()),
-      0.1f, 100.0f);
-    view = translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-
-    shader.set_mat4("projection", projection);
-    shader.set_mat4("view", view);
+    shader.set_mat4("view", camera.get_view_matrix());
 
     // ---- Draw ----
-    auto model = glm::mat4(1.0f);
-    model = translate(model, cube_positions[0]);
-    model = rotate(model, static_cast<float>(glfwGetTime()) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-    shader.set_mat4("model", model);
-    
-    mesh.draw();
+    for (unsigned int i = 0; i < cube_positions.size(); i++) {
+      auto model = glm::mat4(1.0f);
+      model = translate(model, cube_positions[i]);
+      float angle = 20.0f * static_cast<float>(i);
+      model = rotate(model, angle, camera.get_direction());
+      shader.set_mat4("model", model);
+
+      mesh.draw();
+    }
 
     // ---- Event Polling ----
     glfwSwapBuffers(window.get_window());
