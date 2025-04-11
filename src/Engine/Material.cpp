7347ac6bd68ab56818dc24aaa9f42d684819de17
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-04-11 15:25:50 +0400

    Fix: Renamed variables to work with GLSL shaders

--- src/Engine/Material.cpp
+++ src/Engine/Material.cpp
@@ -2,56 +2,53 @@
  * Created by Sarthak Rai on 11 Apr 2025.
 */
 
-#include "../../include/Material.hpp"
+#include "Material.hpp"
 
 namespace STARBORN {
   void Material::apply(const Shader& shader) const {
-    // ---- Set Material Properties ----
-    shader.set_vec4("material.baseColor", base_color);
-    shader.set_float("material.metallic", metallic);
-    shader.set_float("material.roughness", roughness);
-    shader.set_vec3("material.emissiveFactor", emissive_factor);
-    shader.set_float("material.alphaCutoff", alpha_cutoff);
-    shader.set_int("material.alphaModeOpaque", alpha_mode == AlphaMode::OPAQUE ? 1 : 0);
-    shader.set_int("material.alphaModeBlend", alpha_mode == AlphaMode::BLEND ? 1 : 0);
-    shader.set_int("material.alphaModeMask", alpha_mode == AlphaMode::MASK ? 1 : 0);
+    // Set material properties
+    shader.set_vec3("uBaseColor", base_color);
+    shader.set_float("uMetallic", metallic);
+    shader.set_float("uRoughness", roughness);
+    shader.set_vec3("uEmissive", emissive_factor);
+    shader.set_float("uOcclusionStrength", 1.0f); // Adjust as needed
 
-    // ---- Set Texture Flags ----
-    shader.set_int("material.hasBaseColorTexture", base_color_texture ? 1 : 0);
-    shader.set_int("material.hasMetallicRoughnessTexture", metallic_roughness_texture ? 1 : 0);
-    shader.set_int("material.hasNormalTexture", normal_texture ? 1 : 0);
-    shader.set_int("material.hasOcclusionTexture", occlusion_texture ? 1 : 0);
-    shader.set_int("material.hasEmissiveTexture", emissive_texture ? 1 : 0);
+    // Set texture flags
+    shader.set_bool("uUseAlbedoMap", base_color_texture != nullptr);
+    shader.set_bool("uUseMetallicRoughnessMap", metallic_roughness_texture != nullptr);
+    shader.set_bool("uUseNormalMap", normal_texture != nullptr);
+    shader.set_bool("uUseOcclusionMap", occlusion_texture != nullptr);
+    shader.set_bool("uUseEmissiveMap", emissive_texture != nullptr);
 
-    // ---- Bind Textures -----
+    // Bind textures with proper units
     if (base_color_texture) {
       glActiveTexture(GL_TEXTURE0);
       base_color_texture->bind();
-      shader.set_int("material.baseColorTexture", 0);
+      shader.set_int("uAlbedoMap", 0);
     }
 
     if (metallic_roughness_texture) {
       glActiveTexture(GL_TEXTURE1);
       metallic_roughness_texture->bind();
-      shader.set_int("material.metallicRoughnessTexture", 1);
+      shader.set_int("uMetallicRoughnessMap", 1);
     }
 
     if (normal_texture) {
       glActiveTexture(GL_TEXTURE2);
       normal_texture->bind();
-      shader.set_int("material.normalTexture", 2);
+      shader.set_int("uNormalMap", 2);
     }
 
     if (occlusion_texture) {
       glActiveTexture(GL_TEXTURE3);
       occlusion_texture->bind();
-      shader.set_int("material.occlusionTexture", 3);
+      shader.set_int("uOcclusionMap", 3);
     }
 
     if (emissive_texture) {
       glActiveTexture(GL_TEXTURE4);
       emissive_texture->bind();
-      shader.set_int("material.emissiveTexture", 4);
+      shader.set_int("uEmissiveMap", 4);
     }
   }
 }
